c6ccc4e79e1a0d043cb04e897657f764ed8e8c8c
Rob Cho <robhcho@gmail.com>
2018-04-19 21:24:03 -0400

    Make search box find matching firstname, lastname, and emails

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,19 +4,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     el: '#app',
     data: {
       leads: [],
+      searchTerm: "",
       events: [],
       time_format: "12/25/17",
       url: "https://www.google.com/"
     },
     mounted: function() {
       $.get('/api/v1/leads.json').success(function(response) {
-        console.log(this);
         this.leads = response;
       }.bind(this));
     },
     methods: {
       moment: function(date) {
         return moment(date);
+      },
+      foundLead: function(inputName) {
+        var foundFirstName = inputName.first_name.toLowerCase().includes(this.searchTerm.toLowerCase());
+        var foundLastName = inputName.last_name.toLowerCase().includes(this.searchTerm.toLowerCase());
+        var foundEmail = inputName.email.toLowerCase().includes(this.searchTerm.toLowerCase());
+        return foundFirstName || foundLastName || foundEmail;
       },
       edit: function(lead) {
         window.location.href = '/leads/' + lead.id + '/edit';
